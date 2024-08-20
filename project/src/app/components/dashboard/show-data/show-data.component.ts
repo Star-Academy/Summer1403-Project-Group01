a5a645096c4af350e78280cbb6aa5dbfc04ee9de
FrOZEn-FurY <h.nazarieh@gmail.com>
2024-08-20 21:00:51 +0330

    Refactored the way upload file wotrks

--- project/src/app/components/dashboard/show-data/show-data.component.ts
+++ project/src/app/components/dashboard/show-data/show-data.component.ts
@@ -5,6 +5,15 @@ import {FormsModule} from "@angular/forms";
 import {HttpClient} from "@angular/common/http";
 import {API_BASE_URL} from "../../../app.config";
 
+interface Transaction {
+  TransactionId: number,
+  sourceAccountId: number,
+  destinationAccountId: number,
+  amount: number,
+  date: string,
+  type: string
+}
+
 @Component({
   selector: 'app-show-data',
   standalone: true,
@@ -16,17 +25,24 @@ import {API_BASE_URL} from "../../../app.config";
 })
 export class ShowDataComponent {
   user!: User | undefined;
+  data: Transaction | undefined = undefined;
+
   @ViewChild('labelElement') labelElement!: ElementRef<HTMLLabelElement>;
   @ViewChild('inputElement') inputElement!: ElementRef<HTMLInputElement>;
+  @ViewChild('selectElement') selectElement!: ElementRef<HTMLSelectElement>;
 
   constructor(private userService: UserService, private http: HttpClient) {
     this.user = this.userService.getUser();
   }
 
-  handleChange() {
+  handleChange(): void {
     if (this?.inputElement?.nativeElement?.files && this?.inputElement?.nativeElement?.files?.length > 0) {
       this.labelElement.nativeElement.textContent = this?.inputElement?.nativeElement?.files[0].name;
     }
+  }
+
+  handleDisabled(): boolean {
+    return !(this?.inputElement?.nativeElement?.files && this?.inputElement?.nativeElement?.files?.length > 0);
   }
 
   onSubmit(): void {
@@ -34,9 +50,19 @@ export class ShowDataComponent {
     if (this.inputElement.nativeElement.files) {
       const file = this.inputElement.nativeElement.files[0];
       formData.append('file', file);
-      this.http.post(API_BASE_URL + 'Transaction/ImportTransactions', formData).subscribe((response) => {
-        console.log(response);
-      })
+      if (this.selectElement.nativeElement.value === "transaction") {
+        this.http.post(API_BASE_URL + 'Transaction/ImportTransactions', formData).subscribe((response) => {
+          console.log(response);
+        })
+      } else if (this.selectElement.nativeElement.value === "account") {
+        this.http.post(API_BASE_URL + 'Account/ImportAccounts', formData).subscribe((response) => {
+          console.log(response);
+        })
+      }
     }
+  }
+
+  updateData(): void {
+
   }
 }
