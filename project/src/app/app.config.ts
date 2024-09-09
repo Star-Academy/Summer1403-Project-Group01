c2299e9bbc4dcf2c19fd75c400d0c36678d3909b
FrOZEn-FurY <h.nazarieh@gmail.com>
2024-09-09 15:16:38 +0330

    feat: added the features needed

--- project/src/app/app.config.ts
+++ project/src/app/app.config.ts
@@ -18,4 +18,4 @@ export const appConfig: ApplicationConfig = {
   ],
 };
 
-export const API_BASE_URL = 'http://external.abriment.com:30080/';
+export const API_BASE_URL = 'https://mybackend.abriment.com/';
